90159cb54a40a3b78deaced6cd63a959a4d89f0e
Priyanshu Raj <priyanshuraj583@gmail.com>
2025-05-15 14:51:00 +0530

    Revert "First PR (#1)"
    This reverts commit b9a230a1f8f5cad78abd9f7eec146f86d343a68c.

--- src/main/java/com/webstore/constant/DatabaseConstants.java
+++
@@ -1,5 +0,0 @@
-package com.webstore.constant;
-
-public class DatabaseConstants {
-    public static final String SCHEMA_NAME = "web_store";
-}
--- src/main/java/com/webstore/validation/UserValidation.java
+++
@@ -1,4 +0,0 @@
-package com.webstore.validation;
-
-public interface UserValidation {
-}
